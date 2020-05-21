77d67859785922a8d4dcafe07bf0d194193f7675
josephar <josephar@umich.edu>
2020-05-20 21:59:13 -0400

    initial work on authenticating logins

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.get("/login", (req, res)  =>{
         user:'root',
         database: 'local_database'
     });
-
-    const query_string = "SELECT COUNT(*) FROM users WHERE username ='?' and password =?"
+    
+    const username = req.params.username;
+    const password = req.params.password;
+    const query_string = "SELECT COUNT(*) FROM users WHERE username = ? and password = ?"
+    connection.query(query_string, [username, password], (err, rows, field) =>{
+        console.log("authenticating");
+        //need some response
+    });
 });
