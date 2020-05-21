c4e3164f12ab77628f89fad61b5b01dd3cc697c4
josephar <josephar@umich.edu>
2020-05-20 21:55:06 -0400

    started work on authenticating a login in the database

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.get("/", (req, res) =>{
 app.listen(3000, () => {
     console.log("Server is listening");
 }); 
+
+app.get("/login", (req, res)  =>{
+    console.log("fetching user with username: " + req.params.id);
+
+    const connection = mysql.createConnection({
+        host: '127.0.0.1',
+        user:'root',
+        database: 'local_database'
+    });
+
+    const query_string = "SELECT COUNT(*) FROM users WHERE username ='?' and password =?"
+});
