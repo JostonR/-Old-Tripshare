bdcfd5f426dad5db4c5a881001d91232f76c4a69
josephar <josephar@umich.edu>
2020-05-22 00:56:43 -0400

    added additional layer of file abstraction for node access. MySQL linked on html pages.. Still throwing error and not connecting to database yet

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
+const body_parser = require('body-parser');
+
+app.use(express.static('./pages'));
+app.use(body_parser.urlencoded({extended: false}));
+
+function get_connection(){
+    
+    return mysql.createConnection({
+        host: '127.0.0.1',
+        user:'root',
+        password:'Anthony99',
+        database: 'database1'
+    });
+};
+
+
+
 app.get("/", (req, res) =>{
     console.log("Responding to root route");
     res.send("hello");
-})
+});
 
 app.listen(3000, () => {
     console.log("Server is listening");
@@ -14,17 +31,56 @@ app.listen(3000, () => {
 app.get("/login", (req, res)  =>{
     console.log("fetching user with username: " + req.params.id);
 
-    const connection = mysql.createConnection({
-        host: '127.0.0.1',
-        user:'root',
-        database: 'local_database'
-    });
+    const connection = get_connection();
     
-    const username = req.params.username;
-    const password = req.params.password;
-    const query_string = "SELECT COUNT(*) FROM users WHERE username = ? and password = ?"
-    connection.query(query_string, [username, password], (err, rows, field) =>{
+    const user_username = req.body.user_username;
+    const user_password = req.body.user_password;
+    const query_string = "SELECT password FROM users WHERE username = ?"
+    connection.query(query_string, [username], (err, results, fields) =>{
         console.log("authenticating");
+
+        if(err){
+            console.log("error");
+            res.sendStatus(500);
+            return;
+        }
+
+        console.log("inserted a new user");
         //need some response
+    });
+
+    res.end();
+});
+
+app.post("/signup", (req, res) => {
+    console.log("creating user");
+    const user_username = req.body.create_username;
+    const user_password = req.body.create_password;
+    const query_string = "INSERT INTO users (username, password) VALUES (?, ?)";
+    get_connection().query(query_string, [user_username, user_password], (err, results, fields) =>{
+        if(err){
+            console.log("error");
+            res.sendStatus(500);
+            return;
+        }
+        else{
+            res.send("new user created");
+        }
+    });
+});
+
+app.get("/user", (req, res) =>{
+    console.log("listing connections");
+    const connection = get_connection();
+    const user_name = req.body.db_test;
+
+    const query_string = "SELECT * FROM users WHERE username = Joston99";
+    connection.query(query_string, [user_name], (err, rows, fields) =>{
+        if(err){
+            console.log("error");
+            res.sendStatus(500);
+            return;
+        }
+        res.send("hello");
     });
 });
