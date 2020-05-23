25c40babcd75c4ced669548ca0007620aab02f3c
vanbuselvan <34636163+vanbuselvan@users.noreply.github.com>
2020-05-23 17:03:54 -0400

    EJS, MySQL and Express set up

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+/*var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "john",
+    password: "Pass1234",
+    database: "testdb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM people", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});*/
+
 app.get("/", (req, res) =>{
     console.log("Responding to root route");
     res.send("hello");
+})
+
+app.get("/login", (req, res) =>{
+    //res.send("login page tbd");
+    res.render("homepage.html", {uname: "Vishnu"});
 })
 
 app.listen(3000, () => {
